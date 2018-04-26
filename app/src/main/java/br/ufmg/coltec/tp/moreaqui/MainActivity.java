3395c745321591c9144c55471bccedcf7d6e91c1
ritarez <ritarezborges@gmail.com>
2018-04-26 11:25:50 -0300

    if(valor.getText() == null || valor.getText() == )

--- app/src/main/java/br/ufmg/coltec/tp/moreaqui/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/moreaqui/MainActivity.java
@@ -1,10 +1,16 @@
 package br.ufmg.coltec.tp.moreaqui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.ListView;
-
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class MainActivity extends Activity {
 
@@ -17,8 +23,68 @@ public class MainActivity extends Activity {
         // recupera os imóveis cadastrados no DAO até o momento e os carrega na lista
         ImovelDAO dao = ImovelDAO.getInstance();
         this.atualizarLista(dao.getImoveis());
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu m){
+        getMenuInflater().inflate(R.menu.menu_add, m);
+        super.onCreateOptionsMenu(m);
+
+        // busca pelo SearchView e customiza suas ações
+        MenuItem item = m.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                ImovelDAO dao = ImovelDAO.getInstance();
+                ArrayList<Imovel> array = dao.filtrarImoveis(s);
+                atualizarLista(array);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                // executado enquanto texto é alterado pelo usuário
+                return false;
+            }
+        });
+
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                ImovelDAO dao = ImovelDAO.getInstance();
+                atualizarLista(dao.getImoveis());
+                return false;
+            }
+        });
+
+
+        return true;
     }
 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // recupera id do item selecionado
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_add:
+                Intent intent = new Intent(MainActivity.this, NovoImovelActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        ImovelDAO dao = ImovelDAO.getInstance();
+        atualizarLista(dao.getImoveis());
+    }
 
     /**
      * Método auxiliar para atualizar a ListView. Utilizar quando os dados da lista
@@ -29,6 +95,7 @@ public class MainActivity extends Activity {
      * @param imoveis lista que será enviada ao adapter
      */
     private void atualizarLista(ArrayList<Imovel> imoveis) {
+
         ListView imoveisList = findViewById(R.id.imoveisList);
         imoveisList.setAdapter(new ImoveisAdapter(this, imoveis));
     }
---
+++ app/src/main/java/br/ufmg/coltec/tp/moreaqui/NovoImovelActivity.java
@@ -0,0 +1,58 @@
+package br.ufmg.coltec.tp.moreaqui;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NovoImovelActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_imovel);
+
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Button btn = findViewById(R.id.save);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText nome = findViewById(R.id.nome);
+                EditText end = findViewById(R.id.end);
+                EditText cidade = findViewById(R.id.cidade);
+                EditText valor = findViewById(R.id.valor);
+                EditText tel = findViewById(R.id.tel);
+
+                String nomeTxt = nome.getText().toString();
+                String endTxt = end.getText().toString();
+                String cidadeTxt = cidade.getText().toString();
+
+                Double valorTxt = 0.0;
+
+                if(!valor.getText().toString().equals(""))
+                    valorTxt =  Double.valueOf(valor.getText().toString());
+
+                String telTxt = tel.getText().toString();
+
+
+                //Adiciona ao banco de dados
+
+                ImovelDAO dao = ImovelDAO.getInstance();
+                dao.adicionarImovel(new Imovel(nomeTxt, endTxt, cidadeTxt, valorTxt, telTxt));
+                finish();
+            }
+        });
+    }
+
+}
