9d288fb442d1a2b714d313b9b7d450947a65239a
MariSantos17 <mariana@luarinformatica.com.br>
2018-04-26 11:13:11 -0300

    Prova

--- app/src/main/java/br/ufmg/coltec/tp/moreaqui/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/moreaqui/MainActivity.java
@@ -1,7 +1,12 @@
 package br.ufmg.coltec.tp.moreaqui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,10 +19,42 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        Button Add = findViewById(R.id.);
+//
+//        Add.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this,NovoImovelActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+
+
+
         // recupera os imóveis cadastrados no DAO até o momento e os carrega na lista
         ImovelDAO dao = ImovelDAO.getInstance();
         this.atualizarLista(dao.getImoveis());
     }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//
+//        MenuItem item = menu.findItem(R.id.action_add);
+//
+//        item.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this,NovoImovelActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        return super.onCreateOptionsMenu(menu);
+//    }
+
 
 
     /**
@@ -29,7 +66,9 @@ public class MainActivity extends Activity {
      * @param imoveis lista que será enviada ao adapter
      */
     private void atualizarLista(ArrayList<Imovel> imoveis) {
-        ListView imoveisList = findViewById(R.id.imoveisList);
-        imoveisList.setAdapter(new ImoveisAdapter(this, imoveis));
+        ListView imoveisListView = findViewById(R.id.imoveisList);
+        imoveisListView.setAdapter(new ImoveisAdapter(this, imoveis));
+
+
     }
 }
