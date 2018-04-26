b83d271aee1e15689d79f822309652564a459fc5
joao vitor da silva braga <a2016951723@la0012217565.infocoltec.local>
2018-04-26 11:46:17 -0300

    Segunda tentativa

--- app/src/main/java/br/ufmg/coltec/tp/moreaqui/ImovelDAO.java
+++ app/src/main/java/br/ufmg/coltec/tp/moreaqui/ImovelDAO.java
@@ -89,9 +89,12 @@ public class ImovelDAO {
         // Dica: Para implementar a filtragem, você deverá verificar se o parâmetro "nome"
         // está incluso dentro do nome de cada imóvel (Utilize o String.contains)
 
+        for (int i=0; i<imoveis.size();i++){
+            if (imoveis.get(i).getNome().contains(nome)){
+                imoveisFiltrados.add(imoveis.get(i));
 
-
-
+            }
+        }
 
 
 
