829221cf4961d9235077fb750cc3568ce45c447e
veazey <gabrielveazey7@gmail.com>
2018-04-26 11:24:35 -0300

    Upando Prova triste :(

--- app/src/main/java/br/ufmg/coltec/tp/moreaqui/ImovelDAO.java
+++ app/src/main/java/br/ufmg/coltec/tp/moreaqui/ImovelDAO.java
@@ -42,37 +42,37 @@ public class ImovelDAO {
         imoveis.add(new Imovel("Flat Savassi",
                 "Av. Getúlio Vargas, 3500, Savassi",
                 "Belo Horizonte",
-                3000.00,
+                "3000.00 BRL",
                 "2314-1514"));
 
         imoveis.add(new Imovel("Moradia UFMG",
                 "Flemming",
                 "Belo Horizonte",
-                850.00,
+                "850.00 BRL",
                 "3409-2525"));
 
         imoveis.add(new Imovel("Edifício JK",
                 "Av. Olegário Maciel, 500, Centro",
                 "Belo Horizonte",
-                1200.00,
+                "1200.00 BRL",
                 "99456-4430"));
 
         imoveis.add(new Imovel("Casa 3 quartos",
                 "Rua Santa Mônica, 450, Ouro Preto",
                 "Belo Horizonte",
-                1800.00,
+                "1800.00 BRL",
                 "3411-3030"));
 
         imoveis.add(new Imovel("Eldorado",
                 "Praça da CEMIG, 500",
                 "Contagem",
-                800.00,
+                "800.00 BRL",
                 "3350-0670"));
 
         imoveis.add(new Imovel("Mansão Mangabeiras",
                 "Av. Afonso Pena, 13000",
                 "Belo Horizonte",
-                14000.00,
+                "14000.00 BRL",
                 "99999-9999"));
     }
 
